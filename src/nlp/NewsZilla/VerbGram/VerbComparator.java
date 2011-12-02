7aa63d99b9e199e4c5d89999a0628f0c71f5a938
Porter <pwestling@gmail.com>
2011-12-02 14:08:32 -0500

    Fixed bug in VerbComparator: It wasn't taking into account that indexOf return -1 if object is not found in list

--- src/nlp/NewsZilla/VerbGram/VerbComparator.java
+++ src/nlp/NewsZilla/VerbGram/VerbComparator.java
@@ -5,22 +5,27 @@ import java.util.Comparator;
 import java.util.Map.Entry;
 
 public class VerbComparator implements Comparator<Entry<String, String>> {
-	
-	static final ArrayList<String> verbOrder = new ArrayList<String>() {{
-		add("VB");
-		add("VBN");
-		add("VBD");
-		add("VBP");
-		add("VBZ");
-		add("VBG");
-		
-	}};
+
+	static final ArrayList<String> verbOrder = new ArrayList<String>() {
+		{
+			add("VB");
+			add("VBN");
+			add("VBD");
+			add("VBP");
+			add("VBZ");
+			add("VBG");
+
+		}
+	};
 
 	@Override
 	public int compare(Entry<String, String> v1, Entry<String, String> v2) {
 		Integer rankV1 = verbOrder.indexOf(v1.getValue());
 		Integer rankV2 = verbOrder.indexOf(v2.getValue());
+		if (rankV1 == -1)
+			rankV1 = 999;
+		if (rankV2 == -1)
+			rankV2 = 999;
 		return rankV1.compareTo(rankV2);
 	}
-
 }
